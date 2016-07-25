e2b1572783a6a956926163fe1d4b6f5bb233e443
Gordon <kazrah@gmail.com>
2016-07-25 09:06:31 -0700

    commit of all project files

---
+++ app/src/main/java/com/gordonzeng/sharedpreferencesdemo/MainActivity.java
@@ -0,0 +1,67 @@
+package com.gordonzeng.sharedpreferencesdemo;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.gordonzeng.sharedpreferencesdemo", Context.MODE_PRIVATE);
+
+        sharedPreferences.edit().putString("username", "gordon").apply();
+
+        String username = sharedPreferences.getString("username", "");
+
+        Log.i("username", username);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure?")
+                .setMessage("Do you definitely want to do this?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Button Tapped", "Yes");
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add) {
+
+            Log.i("button tapped", "Add");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
